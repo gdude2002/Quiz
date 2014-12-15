975bbf16fa8aab88a70193fed722d990b06a413b
Daniel <daniel@daniel.daniel>
2014-12-15 11:11:33 +0000

    Start of the questions storage

--- src/main/java/com/github/quiz/Questions.java
+++ src/main/java/com/github/quiz/Questions.java
@@ -28,6 +28,92 @@ public class Questions {
                 }
             }
          */
+
+        this.addQuestion(
+                "Who holds the record for the most losses in total in the English Soccer Leagues History?",  // Question
+                3,  // Correct index
+                "Burnley", "Sunderland", "Norwich", " Everton"  // Answers
+        );
+
+        /* `this.questions` now looks like this:
+
+            this.questions = {
+                 "Who holds the record for the most losses in total in the English Soccer Leagues History?"": {
+                    "answers": [
+                        "Burnley", "Sunderland", "Norwich", "Everton"
+                    ],
+                    "correct": "Everton"
+                }
+            }
+         */
+        this.addQuestion(
+                "Who is the world cups all time top goal scorer?",  // Question
+                2,  // Correct index
+                "Pele", "Ronaldo", "Miroslav Klose", "Gerd Mueller"  // Answers
+        );
+
+        /* `this.questions` now looks like this:
+
+            this.questions = {
+                 "Who holds the record for the most losses in total in the English Soccer Leagues History?"": {
+                    "answers": [
+                        "Pele", "Ronaldo", "Miroslav Klose", "Gerd Mueller"
+                    ],
+                    "correct": "Miroslav Klose"
+                }
+            }
+         */
+        this.addQuestion(
+                " Who holds the record for most Premier League goals??",  // Question
+                0,  // Correct index
+                "Alan Shearer", "Andrew Cole", "Wayne Rooney", "Thierry Henry"  // Answers
+        );
+
+        /* `this.questions` now looks like this:
+
+            this.questions = {
+                 "Who holds the record for most Premier League goals??"": {
+                    "answers": [
+                       "Alan Shearer", "Andrew Cole", "Wayne Rooney", "Thierry Henry"
+                    ],
+                    "correct": "Alan Shearer"
+                }
+            }
+         */
+        this.addQuestion(
+                " How many clubs have won the Champions League 5 or more times?",  // Question
+                2,  // Correct index
+                "2", "3", "4", "5"  // Answers
+        );
+
+        /* `this.questions` now looks like this:
+
+            this.questions = {
+                 " How many clubs have won the Champions League 5 or more times?": {
+                    "answers": [
+                       "2", "3", "4", "5" , "Thierry Henry"
+                    ],
+                    "correct": "Alan Shearer"
+                }
+            }
+         */
+        this.addQuestion(
+                "Which english club knocked both Liverpool F.C. and Chealsea F.C. out of the 2007/2008 FA Cup?",  // Question
+                3,  // Correct index
+                " Bradford", "Burnley", "Manchester United", "Barnsley"  // Answers
+        );
+
+        /* `this.questions` now looks like this:
+
+            this.questions = {
+                 " How many clubs have won the Champions League 5 or more times?": {
+                    "answers": [
+                       "2", "3", "4", "5" , "Thierry Henry"
+                    ],
+                    "correct": "Alan Shearer"
+                }
+            }
+         */
     }
 
     /**
