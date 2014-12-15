6cb383092b6e1c0b3a7c8248fe9086ba9c7261a8
Gareth Coles <gdude2002@gmail.com>
2014-12-15 09:44:36 +0000

    GUI and gradle run

---
+++ src/main/java/com/github/quiz/GUI.java
@@ -0,0 +1,13 @@
+package com.github.quiz;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI () {
+        setTitle("Simple example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
--- src/main/java/com/github/quiz/Main.java
+++ src/main/java/com/github/quiz/Main.java
@@ -1,11 +1,19 @@
 package com.github.quiz;
 
+import java.awt.EventQueue;
+
 /**
  * Application entry point
  */
 public class Main {
 
     public static void main (String ... args) {
-
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GUI gui = new GUI();
+                gui.setVisible(true);
+            }
+        });
     }
 }
